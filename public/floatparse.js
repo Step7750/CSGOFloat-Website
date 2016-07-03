aecad1354d2f204ad46e539fa519ac77c0bda4b8
Step7750 <Step7750@gmail.com>
2016-07-02 19:24:53 -0600

    Changed API server

--- public/floatparse.js
+++ public/floatparse.js
@@ -1,4 +1,4 @@
-var socket = io.connect('192.168.1.79:1738');
+var socket = io.connect('api.csgofloat.com:1738');
 
 function lookup() {
   var lookup_string = decodeURIComponent($('#input_url').val());

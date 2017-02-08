6b88f6db5dae995eb34296ee617b7ef968771c9c
Step7750 <Step7750@gmail.com>
2017-02-07 23:58:24 -0700

    Restructures JS + HTML
    Probably should also restructure all the crazy styling happening
    everywhere.

--- public/floatparse.js
+++ public/floatparse.js
@@ -1,170 +1,154 @@
-var socket = io.connect('https://api.csgofloat.com:1738', {secure: true});
+const socket = io.connect('https://api.csgofloat.com:1738', {secure: true});
 
-function lookup() {
-  var lookup_string = decodeURIComponent($('#input_url').val());
+const wear_ranges = [[0.00, 0.07], [0.07, 0.15], [0.15, 0.38], [0.38, 0.45], [0.45, 1.00]];
+const wear_names = ['Factory New', 'Minimal Wear', 'Field-Tested', 'Well-Worn', 'Battle-Scarred'];
 
-  // Check proper structure client side
-  var regexed = lookup_string.match(/^steam:\/\/rungame\/730\/\d+\/\+csgo_econ_action_preview [SM]\d+[A]\d+[D]\d+$/g);
-
-  if (regexed != null && regexed == lookup_string) {
-    // this is most likely a valid inspect link structure (afaik, still to be confirmed)
-    //console.log("sending lookup");
-    socket.emit('lookup', lookup_string);
-    $('#input_url').val('');
-  }
-  else {
-    data = '<div class="alert alert-info" role="alert">You must insert a valid inspect link of the form <br>steam://rungame/730/xxxxxxxxxxx/+csgo_econ_action_preview AxxxxxxxxBxxxxxxxxCxxxxxxxx</div>';
-    $(data).hide().prependTo("#messages").slideDown("slow");
-    $('#input_url').val('');
-  }
-}
-
-var data = '<div class="alert alert-info" role="alert">Trying to join the API server...</div>';
-$(data).hide().prependTo("#messages").slideDown("slow");
-
-
-socket.on("joined", function() {
-    data = '<div class="alert alert-success" role="alert">Successfully joined the server</div>';
-    $(data).hide().prependTo("#messages").slideDown("slow");
+$('#input_url').keypress((e) => {
+    if (e.which === 13) {
+        lookup();
+        e.preventDefault();
+    }
 });
 
-socket.on("successmessage", function(msg) {
-    data = '<div class="alert alert-success" role="alert">' + msg['msg'] +'</div>';
-    $(data).hide().prependTo("#messages").slideDown("slow");
+let data = '<div class="alert alert-info" role="alert">Trying to join the API server...</div>';
+$(data).hide().prependTo('#messages').slideDown('slow');
+
+socket.on('joined', () => {
+    let data = '<div class="alert alert-success" role="alert">Successfully joined the server</div>';
+    $(data).hide().prependTo('#messages').slideDown('slow');
 });
 
-socket.on("infomessage", function(msg) {
-    data = '<div class="alert alert-info" role="alert">' + msg['msg'] +'</div>';
-    $(data).hide().prependTo("#messages").slideDown("slow");
+socket.on('successmessage', (msg) => {
+    let data = `<div class="alert alert-success" role="alert">${msg.msg}</div>`;
+    $(data).hide().prependTo('#messages').slideDown('slow');
 });
 
-socket.on("warningmessage", function(msg) {
-    data = '<div class="alert alert-warning" role="alert">' + msg['warning'] +'</div>';
-    $(data).hide().prependTo("#messages").slideDown("slow");
+socket.on('infomessage', (msg) => {
+    let data = `<div class="alert alert-info" role="alert">${msg.msg}</div>`;
+    $(data).hide().prependTo('#messages').slideDown('slow');
 });
 
-socket.on("errormessage", function(msg) {
-    data = '<div class="alert alert-danger" role="alert">' + msg['error'] +'</div>';
-    $(data).hide().prependTo("#messages").slideDown("slow");
+socket.on('errormessage', (msg) => {
+    let data = `<div class="alert alert-danger" role="alert">${msg.error}</div>`;
+    $(data).hide().prependTo('#messages').slideDown('slow');
 });
 
-socket.on('disconnect', function() {
-  data = '<div class="alert alert-danger" role="alert">Disconnected from server, trying to reconnect</div>';
-  $(data).hide().prependTo("#messages").slideDown("slow");
+socket.on('disconnect', () => {
+    let data = '<div class="alert alert-danger" role="alert">Disconnected from server, trying to reconnect</div>';
+    $(data).hide().prependTo('#messages').slideDown('slow');
 });
 
-wear_ranges = [[0.00, 0.07], [0.07, 0.15], [0.15, 0.38], [0.38, 0.45], [0.45, 1.00]];
-wear_names = ["Factory New", "Minimal Wear", "Field-Tested", "Well-Worn", "Battle-Scarred"];
+socket.on('floatmessage', (itemdata) => {
+    itemdata = itemdata.iteminfo;
 
-socket.on("floatmessage", function(itemdata) {
-    itemdata = itemdata["iteminfo"];
-    console.log(itemdata);
-
-    min = parseFloat(itemdata["min"]);
-    max = parseFloat(itemdata["max"]);
+    let min = parseFloat(itemdata.min);
+    let max = parseFloat(itemdata.max);
 
     // find out the wear range
-    wear_range = [0, 0];
+    let wear_range = [0, 0];
+    let float_val = itemdata.floatvalue;
 
-    offset = [0, 0] // percentage to offset the bar
-    wear_name = "";
-    float_offset = 0
+    // Calculate offsets and wear range
+    for (let i in wear_ranges) {
+        let lower_bound = wear_ranges[i][0];
+        let upper_bound = wear_ranges[i][1];
 
-    float_val = itemdata["floatvalue"];
+        if (float_val >= lower_bound && float_val < upper_bound) {
+            wear_range[0] = lower_bound < min ? min : lower_bound;
+            wear_range[1] = upper_bound > max ? max : upper_bound;
 
-    for(var i in wear_ranges) {
-      if (wear_ranges[i][0] < float_val && float_val < wear_ranges[i][1]) {
+            var wear_name = wear_names[i];
+            var float_offset = ((float_val - wear_range[0]) / (wear_range[1] - wear_range[0])) * 100;
 
-        wear_name = wear_names[i];
-        if (wear_ranges[i][0] < min) {
-          wear_range[0] = min;
-          offset[0] = ((min - wear_ranges[i][0]) / (wear_ranges[i][1] - wear_ranges[i][0])) * 100;
+            break;
         }
-        else {
-          wear_range[0] = wear_ranges[i][0];
-        }
-
-        if (wear_ranges[i][1] > max) {
-          wear_range[1] = max;
-          offset[1] = ((max - wear_ranges[i][0]) / (wear_ranges[i][1] - wear_ranges[i][0])) * 100;
-        }
-        else {
-          wear_range[1] = wear_ranges[i][1];
-        }
-        //console.log(wear_range);
-        float_offset = ((float_val - wear_range[0]) / (wear_range[1] - wear_range[0])) * 100;
-        break;
-      }
     }
-    progress_bar = '<div style="position: relative;"><div class="progress"><div class="progress-bar progress-bar-success progress-bar-striped active" style="margin-left: 0%; width: 100%;">' + wear_name + '</div></div>';
-    progress_bar += '<div style="width: 5px; height: 20px; position: absolute; background: red; top: 0; left: ' + float_offset + '%;" title="' + float_val +'"></div>'
-    progress_bar += '<div style="position: absolute; top: 20px; left: 0;">' + wear_range[0] + '</div>';
-    progress_bar += '<div style="position: absolute; top: 20px; right: 0;">' + wear_range[1] + '</div>';
 
-    progress_bar += '</div>'
-    data = '<div class="alert alert-info" role="alert" style="min-height: 150px;" id="' + itemdata["itemid_int"] + '_success">'+ progress_bar 
+    let itemname = `${generateItemName(itemdata)} (${wear_name})`;
 
-    itemname = generateItemName(itemdata) + " (" + wear_name + ")";
+    let data = `
+        <div class='alert alert-info' role='alert' style='min-height: 150px;' id='${itemdata.itemid_int}_success'>
+            <div style='position: relative;'>
+                <div class='progress'>
+                    <div class='progress-bar progress-bar-success progress-bar-striped active' style='margin-left: 0%; width: 100%;'>
+                        ${wear_name}
+                    </div>
+                </div>
+                <div style='width: 5px; height: 20px; position: absolute; background: red; top: 0; left: ${float_offset}%;' title='${float_val}'></div>
+                <div style='position: absolute; top: 20px; left: 0;'>${wear_range[0].toFixed(2)}</div>
+                <div style='position: absolute; top: 20px; right: 0;'>${wear_range[1].toFixed(2)}</div>
+            </div>
+            ${generateItemHTML(itemdata, itemname, wear_name)}
+        </div>
+    `;
 
-    data += generateItemHTML(itemdata, itemname, wear_name) + '</div>';
+    $(data).hide().prependTo('#messages').show();
 
-
-    $(data).hide().prependTo("#messages").show();
-
-    $("[title]").tooltip({'placement': "bottom"});
+    $('[title]').tooltip({'placement': 'bottom'});
 });
 
-function generateItemHTML(itemdata, itemname, wear_name) {
-  innerHTML = ''
+const lookup = function() {
+    let lookup_string = decodeURIComponent($('#input_url').val());
 
-  if (itemdata["imageurl"] != null) {
-    innerHTML += '<img src="' + itemdata["imageurl"] + '" height="150px" style="float: left; margin-top: -10px;"></br><div style="text-align: left; margin-top: -15px; padding-left: 210px;">';
-  }
-  innerHTML += '<div class="itemname">' + itemname + "</div>";
-  innerHTML += '<div class="float_val">Float Value: ' + itemdata["floatvalue"] + '</div>';
+    // Check proper structure client side
+    let regexed = lookup_string.match(/^steam:\/\/rungame\/730\/\d+\/[+ ]csgo_econ_action_preview [SM]\d+A\d+D\d+$/g);
 
-  innerHTML += 'Paint Index: ' + itemdata["paintseed"];
-  innerHTML += '<br>Item ID: ' + itemdata["itemid_int"];
+    if (regexed && regexed[0] === lookup_string) socket.emit('lookup', lookup_string);
+    else {
+        data = `
+        <div class='alert alert-info' role='alert'>
+            You must insert a valid inspect link of the form <br>
+            steam://rungame/730/xxxxxxxxxxx/+csgo_econ_action_preview AxxxxxxxxBxxxxxxxxCxxxxxxxx
+        </div>`;
 
-  innerHTML += '<br><a href="' + generateInspectURL(itemdata) + '" class="btn btn-info">Inspect in Game</a>';
+        $(data).hide().prependTo('#messages').slideDown('slow');
+    }
 
-  return innerHTML;
+    $('#input_url').val('');
 }
 
-function generateInspectURL(itemdata) {
-  url = "";
+const generateItemHTML = function(itemdata, itemname, wear_name) {
+    let img_html = '';
 
-  if (itemdata["s"] == "0") {
-    url = "steam://rungame/730/76561202255233023/+csgo_econ_action_preview M" + itemdata["m"] + "A" + itemdata["a"] + "D" + itemdata["d"];
-  }
-  else {
-    url = "steam://rungame/730/76561202255233023/+csgo_econ_action_preview S" + itemdata["s"] + "A" + itemdata["a"] + "D" + itemdata["d"];
-  }
+    if (itemdata.imageurl) {
+        img_html = `
+            <img src='${itemdata.imageurl}' height='150px' style='float: left; margin-top: -10px;'>
+            </br>
+            <div style='text-align: left; margin-top: -15px; padding-left: 210px;'>
+        `;
+    }
 
-  return url;
+    return `
+        ${img_html}
+        <div class='itemname'>${itemname}</div>
+        <div class='float_val'>Float Value: ${itemdata.floatvalue}</div>
+        Paint Seed: ${itemdata.paintseed}</br>
+        Item ID: ${itemdata.itemid_int}</br>
+        <a href='${generateInspectURL(itemdata)}' class='btn btn-info'>Inspect in Game</a>
+    `;
 }
 
-function generateItemName(itemdata) {
-  itemname = ""
-  if (itemdata["defindex"] >= 500) {
-    // this is a knife, add the star symbol
-    itemname += "★ ";
-  }
-  if (itemdata["killeatervalue"] != null) {
-    itemname += "StatTrak™ ";
-  }
-  itemname += itemdata["weapon_type"];
-  if (itemdata["item_name"] != "-") {
-    itemname += " | " + itemdata["item_name"];
-  }
-
-  return itemname;
-
+const generateInspectURL = function(item) {
+    if (item.s === '0') return `steam://rungame/730/76561202255233023/+csgo_econ_action_preview M${item.m}A${item.a}D${item.d}`;
+    else return `steam://rungame/730/76561202255233023/+csgo_econ_action_preview S${item.s}A${item.a}D${item.d}`;
 }
 
-$('#input_url').keypress(function (e) {
-  if (e.which == 13) {
-    lookup();
-    e.preventDefault();
-  }
-});
+const generateItemName = function(itemdata) {
+    let itemname = '';
+
+    if (itemdata.defindex >= 500) {
+        // this is a knife, add the star symbol
+        itemname += '★ ';
+    }
+    if (itemdata.killeatervalue !== null) {
+        itemname += 'StatTrak™ ';
+    }
+
+    itemname += itemdata.weapon_type;
+
+    if (itemdata.item_name !== '-') {
+        itemname += ' | ' + itemdata.item_name;
+    }
+
+    return itemname;
+}
